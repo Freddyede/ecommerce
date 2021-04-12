ad7452290a936f04e7b06f087bb336e88e230b97
Patouillard <patouillardfranck3@gmail.com>
2021-04-12 22:17:01 +0200

    gitflow-hotfix-stash: CRUD/Users

---
+++ src/Controller/SuperAdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class SuperAdminController
+ * @package App\Controller
+ * @Route("/superadmin", name="superAdmin_")
+ */
+class SuperAdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+        return $this->render('super_admin/index.html.twig', [
+            'controller_name' => 'SuperAdminController',
+        ]);
+    }
+}
